83d46c6e1ad1294997f342a1e576aff4c2ce5e4e
Daniel Seriy <danielgriz@gmail.com>
2016-10-18 23:45:49 +0300

    Create first URL and view

--- books/admin.py
+++ books/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from .models import Book, Author
 
-@admin.register(Book)
 
+@admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     fieldsets = [
         ("Book Details", {"fields": ["title", "authors"]}),
--- readit/urls.py
+++ readit/urls.py
@@ -13,14 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls import include, url
 
+from books.views import list_books
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', list_books, name='books'),
 ]
 
 if settings.DEBUG:
