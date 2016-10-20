f4abfbb76f1540d1f78bbaf39e2f8d1e76e03ca1
Daniel Seriy <danielgriz@gmail.com>
2016-10-21 00:46:44 +0300

    Create list of authors, author page and book page

--- readit/urls.py
+++ readit/urls.py
@@ -18,11 +18,14 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls import include, url
 
-from books.views import list_books
+from books.views import AuthorDetail, BookDetail, AuthorList, list_books
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', list_books, name='books'),
+    url(r'^authors/$', AuthorList.as_view(), name='authors'),
+    url(r'^books/(?P<pk>[-\w]+)/$', BookDetail.as_view(), name='book-detail'),
+    url(r'^authors/(?P<pk>[-\w]+)/$', AuthorDetail.as_view(), name='author-detail'),
 ]
 
 if settings.DEBUG:
